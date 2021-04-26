626363f467159c9ca03d7a00a7450a7eeb852ac4
KraXen72 <dpelech1@gmail.com>
2021-04-26 12:41:43 +0200

    feat: gen

--- preload.js
+++ preload.js
@@ -3,35 +3,108 @@
 
 const electron = require('electron').remote
 const dialog = electron.dialog
-var maindir
 
+const utils = require('./utils.js')
+
+const walk = require('fs-walk')
+const mm = require('music-metadata');
+const fs = require('fs')
+
+var slash = process.platform === 'win32' ? "\\" : "/"
+var config = utils.initOrLoadConfig("./config.json")
+console.log(config)
+
+/* ui and other handling */
 window.addEventListener('DOMContentLoaded', () => {
     console.log("loaded")
+    if (config.maindir !== "") {selectfolder(null, config)}
     document.getElementById("folder-open").addEventListener("click", selectfolder)
+    document.getElementById("gen").addEventListener("click",() => {setTimeout(() =>{gen()}, 2000)})
 })
 
-async function selectfolder() {
-    pick = await dialog.showOpenDialog({properties: ['openDirectory']})
-    console.log(pick)
-    if (pick.canceled == false) {
-        console.log("didn't cancel")
-        maindir = pick.filePaths[0]
-        document.getElementById("selected-folder").innerText = shortenFilename(maindir, 40)
+async function selectfolder(mouseevent, inputconfig) {
+
+    if (typeof inputconfig === "undefined") { //clicked on the pick button
+        pick = await dialog.showOpenDialog({properties: ['openDirectory']})
+        console.log(pick)
+        if (pick.canceled == false) {
+            console.log("didn't cancel")
+            config.maindir = pick.filePaths[0]
+            utils.saveConfig("./config.json", config)
+        }
+    } else { //loaded from config
+        config.maindir = inputconfig.maindir
     }
-    console.log(maindir)
+
+    document.getElementById("selected-folder").innerText = utils.shortenFilename(config.maindir.toString(), 40)
+    document.getElementById("gen").removeAttribute("disabled")
 }
 
-function shortenFilename(str, len) {
-    if (str.length > len) {
-        let halfsize = Math.floor(len/2)-1 //the final size for each half
-        let firsthalf = str.slice(0, (str.length/2)+1)
-        let secondhalf = str.slice((str.length/2), str.length)
+/*playlist handling*/
 
-        //trim first half
-        if (firsthalf.length > halfsize) { firsthalf = firsthalf.slice(0, halfsize)}
-        //trim second half
-        if (secondhalf.length > halfsize) {secondhalf = secondhalf.slice(secondhalf.length-halfsize, secondhalf.length+1)}
+//walk all directories and then call generateM3U()
+async function gen() {
+    let alldirs = []
 
-        return firsthalf+"..."+secondhalf
-    } else {return str}
+    walk.dirsSync(config.maindir, (basedir, filename, stats) => {
+        alldirs.push({basedir, filename, "fullpath": basedir + slash + filename, stats})
+    })
+    //console.log(alldirs)
+    alldirs.forEach((dir) => {generateM3U(dir.fullpath, true)})
+    //await generateM3U(alldirs[22].fullpath, true)
+    console.log("done")
+    
+}
+
+//generate a m3u for given folder
+async function generateM3U(folder, useEXTINF) {
+    const allsongs = []
+    if (useEXTINF) {allsongs.push("#EXTM3U")}
+    let relativedir = folder.split(slash)
+    relativedir = relativedir[relativedir.length -1]
+
+    let walksongs = []
+
+    walk.filesSync(folder, (basedir, filename) => {
+        walksongs.push({basedir, filename})
+    })
+
+    for (let i = 0; i < walksongs.length; i++) {
+        const walksong = walksongs[i];
+        let filename = walksong.filename
+        let basedir = walksong.basedir
+
+        song = filename
+        extarr = song.split(".")
+        //if the song extension is in allowed list
+        if (config.exts.includes(extarr[extarr.length - 1])) {   
+            if (useEXTINF == true) {
+                let extinf = await getEXTINF(basedir + slash + song, song)
+                allsongs.push(extinf.toString())
+                allsongs.push(filename.toString())
+            } else {
+                allsongs.push(filename.toString())
+            }
+        }
+    }
+
+    //console.log(allsongs)
+    let lines = allsongs.join("\n")
+
+    fs.writeFileSync(`${folder + slash + relativedir}.m3u`, lines)
+}
+
+//read the file and get it's metadata
+async function getEXTINF(song, onlysong) {
+    const metadata = await mm.parseFile(song, {"skipCovers": true, "duration": false})
+    //console.log(metadata)
+
+    const artist = metadata.common.artist == undefined ? "Unknown Artist" : metadata.common.artist
+    const title = metadata.common.title == undefined ? onlysong : metadata.common.title
+    const duration = metadata.format.duration == undefined || parseInt(metadata.format.duration) < 1 ? "000001" : metadata.format.duration.toFixed(3).replaceAll(".","")
+    const extinf = `#EXTINF:${duration},${artist} - ${title}`
+
+    //console.log(extinf)
+
+    return extinf
 }
---
+++ utils.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+
+
+function shortenFilename(str, len) {
+    if (str.length > len) {
+        let halfsize = Math.floor(len/2)-1 //the final size for each half
+        let firsthalf = str.slice(0, (str.length/2)+1)
+        let secondhalf = str.slice((str.length/2), str.length)
+
+        //trim first half
+        if (firsthalf.length > halfsize) { firsthalf = firsthalf.slice(0, halfsize)}
+        //trim second half
+        if (secondhalf.length > halfsize) {secondhalf = secondhalf.slice(secondhalf.length-halfsize, secondhalf.length+1)}
+
+        return firsthalf+"..."+secondhalf
+    } else {return str}
+}
+
+function initOrLoadConfig(filename) {
+    let config = {}
+
+    if (fs.existsSync(filename)) {
+        config = JSON.parse(fs.readFileSync(filename))
+    } else {
+        config = {
+            "maindir": "",
+            "exts": ["mp3"]
+        }
+        fs.writeFileSync(filename, JSON.stringify(config, null, 2))
+    }
+    return config
+}
+
+function saveConfig(filename, config) {
+    fs.writeFileSync(filename, JSON.stringify(config, null, 2))
+}
+
+module.exports = {shortenFilename, initOrLoadConfig, saveConfig}
