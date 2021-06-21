d757eb0e3eb2a2feaa74e59b23f820c931989cf7
KraXen72 <dpelech1@gmail.com>
2021-06-21 17:04:27 +0200

    add zeropad to utils

--- utils.js
+++ utils.js
@@ -57,4 +57,10 @@ function getExtOrFn(filename) {
     return {fn, ext}
 }
 
-module.exports = {shortenFilename, initOrLoadConfig, saveConfig, fixQuotes, clearFolder, getExtOrFn}
+function zeropad(number, finalWidth, customCharacter) {
+    customCharacter = customCharacter || '0';
+    number = number + '';
+    return number.length >= finalWidth ? number : new Array(finalWidth - number.length + 1).join(customCharacter) + number;
+  }
+
+module.exports = {shortenFilename, initOrLoadConfig, saveConfig, fixQuotes, clearFolder, getExtOrFn, zeropad}
