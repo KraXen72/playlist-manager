3cd335c7dc7d02e0a19f8ec9728ee1b8674ac62a
KraXen72 <dpelech1@gmail.com>
2021-06-21 19:10:54 +0200

    add comments to utils

--- utils.js
+++ utils.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const slash = process.platform === 'win32' ? "\\" : "/"
 
 
-function shortenFilename(str, len) {
+function shortenFilename(str, len) { //turn a long string to start of string...end of string
     if (str.length > len) {
         let halfsize = Math.floor(len/2)-1 //the final size for each half
         let firsthalf = str.slice(0, (str.length/2)+1)
@@ -17,7 +17,7 @@ function shortenFilename(str, len) {
     } else {return str}
 }
 
-function initOrLoadConfig(filename) {
+function initOrLoadConfig(filename) { //initialize config.json
     let config = {}
 
     if (fs.existsSync(filename)) {
@@ -34,23 +34,23 @@ function initOrLoadConfig(filename) {
     return config
 }
 
-function saveConfig(filename, config) {
+function saveConfig(filename, config) { //save config.json
     fs.writeFileSync(filename, JSON.stringify(config, null, 2))
     console.log("saved config")
 }
 
-function fixQuotes(str) {
+function fixQuotes(str) { //escape quotes when put in title attribute for example
     return str.replaceAll('"', "&quot;")
 }
 
-function clearFolder(path) {
+function clearFolder(path) { //delete all files in a folder
     let files = fs.readdirSync(path).filter(f => fs.lstatSync(path + slash + f).isFile() )
     files.forEach(f => {
         fs.unlinkSync(path + slash + f)
     })
 }
 
-function getExtOrFn(filename) {
+function getExtOrFn(filename) { //get the extension or filename from "filename.ext" format
     let splitarr = filename.split(".")
     let ext = splitarr[splitarr.length - 1]
     let fn = splitarr.slice(0, splitarr.length - 1).join(".")
