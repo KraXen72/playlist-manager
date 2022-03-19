ac8d98f5710e25417ddf51a5f40340d7f94ee079
KraXen72 <dpelech1@gmail.com>
2022-03-19 22:54:41 +0100

    discard button and improve saving logic

--- src/frontend/components/ButtonBar.svelte
+++ src/frontend/components/ButtonBar.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
     import Button, {Label, Group} from '@smui/button';
     import IconButton from '@smui/icon-button';
-    import { allSongs, config, currPlaylist, maindir, playlistName } from '$common/stores';
+
+    import { config, currPlaylist, maindir, playlistName, playlistOnlyMode } from '$common/stores';
+    import { createEventDispatcher, onDestroy } from 'svelte';
+
     import playlistImg from "$assets/playlist.png"
-    import { createEventDispatcher } from 'svelte';
     const dispatch = createEventDispatcher()
 
     const api = window.api
+    let changesSaved = true
 
     function pick() {
         let temp = api.dialogApi.pickFolder("Pick the main music folder") ?? [$config.maindir]
@@ -15,9 +18,64 @@
     }
 
     function _savePlaylist() {
-        api.currentPlaylist.save($currPlaylist, $maindir, $playlistName, playlistImg)
-        dispatch("refresh", "sidebar")
+        //console.log($playlistOnlyMode.real)
+        function _postSave() {
+            dispatch("refresh", "sidebar")
+            changesSaved = true
+        }
+
+        if ($playlistOnlyMode.real) {
+            api.currentPlaylist.save($currPlaylist, $maindir, $playlistName, playlistImg)
+            $config.comPlaylists[`${$maindir}${api.slash}${$playlistName}.m3u`] = [...$currPlaylist.map(item => {
+                return {
+                    filename: item.filename,
+                    fullpath: item.fullpath,
+                    relativepath: item.relativepath
+                }
+            })]
+            api.saveConfig("./config.json", $config, false)
+            _postSave()
+        } else if ($playlistOnlyMode.real === false && $currPlaylist.every(item => item.type === "playlist")) {
+            //propose saving as playlist only.
+            const question = "Do you want to save as combined playlist?"
+            const details = "Your playlist only contains playlists. Do you want to save it as a combined playlist? Combined playlists can be later regenerated if you add any songs to them."
+            let confirmCombined = api.dialogApi.confirmdialog(question, details)
+            if (confirmCombined) {
+                $playlistOnlyMode.proposed = true
+                _savePlaylist()
+            } else {
+                api.currentPlaylist.save($currPlaylist, $maindir, $playlistName, playlistImg)
+                _postSave()
+            }
+        } else {
+            api.currentPlaylist.save($currPlaylist, $maindir, $playlistName, playlistImg)
+            _postSave()
+        }
     }
+
+    function _discardPlaylist() {
+        if (changesSaved) {
+            $currPlaylist = []
+            $playlistName = ""
+        } else {
+            const question = "Do you want to discard current playlist?"
+            const details = "The current progress/changes will be lost."
+            let confirmDiscard = api.dialogApi.confirmdialog(question, details)
+            if (confirmDiscard) {
+                $currPlaylist = []
+                $playlistName = ""
+            }
+        }   
+    }
+    
+    const unsub = currPlaylist.subscribe((val) => {
+        if (val.length === 0) {
+            changesSaved = true
+        } else {
+            changesSaved = false
+        }
+    })
+    onDestroy(unsub)
 </script>
 
 <div id="button-bar">
@@ -26,9 +84,12 @@
             variant="outlined" 
             class="smui-icon-btn"
             on:click={_savePlaylist}>
-            <Label class="material-icons">save</Label>
+            <Label class="material-icons {`${changesSaved === false ? "btn-danger" : ""}`}">save</Label>
         </Button>
-        <Button variant="outlined" class="smui-icon-btn">
+        <Button 
+            variant="outlined" 
+            class="smui-icon-btn"
+            on:click={_discardPlaylist}>
             <Label class="material-icons">close</Label>
         </Button>
     </Group>
