aba517fb80814f1a37f1685c39da00bab1c2036e
KraXen72 <dpelech1@gmail.com>
2026-03-11 16:17:37 +0100

    fix start maybe 2

--- start.js
+++ start.js
@@ -1,6 +1,12 @@
 #!/usr/bin/env node
-const { exec } = require('child_process')
-// Replace with whatever you use to start your app
-exec('pnpm run start', {
-  cwd: __dirname
+const { spawn } = require('child_process')
+const path = require('path')
+const electron = require('electron')
+
+spawn(electron, [path.join(__dirname, 'main.js')], {
+  cwd: __dirname,
+  stdio: 'inherit',
+  env: { ...process.env, NODE_OPTIONS: '--experimental-sqlite' }
+}).on('error', (err) => {
+  console.error('Failed to start playlist-manager:', err)
 })
